e403e55292a7fb9fc73023c075f12090b3ff6402
Jan Eitzinger <jan.eitzinger@fau.de>
2022-06-17 10:08:31 +0200

    Improve crypto/tls security settings

--- server.go
+++ server.go
@@ -446,6 +446,12 @@ func main() {
 		}
 		listener = tls.NewListener(listener, &tls.Config{
 			Certificates: []tls.Certificate{cert},
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			},
+			MinVersion:               tls.VersionTLS12,
+			PreferServerCipherSuites: true,
 		})
 		log.Printf("HTTPS server listening at %s...", programConfig.Addr)
 	} else {
