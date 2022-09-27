21b03d02b2ade53a65f347b79c57072f14c635f6
Christoph Kluge <christoph.kluge@fau.de>
2022-09-27 10:33:36 +0200

    Swagger definition URL now based on config.json Addr field

--- cmd/cc-backend/main.go
+++ cmd/cc-backend/main.go
@@ -291,7 +291,7 @@ func main() {
 	if flagDev {
 		r.Handle("/playground", playground.Handler("GraphQL playground", "/query"))
 		r.PathPrefix("/swagger/").Handler(httpSwagger.Handler(
-		httpSwagger.URL("http://localhost:8080/swagger/doc.json"))).Methods(http.MethodGet)
+		httpSwagger.URL("http://" + config.Keys.Addr + "/swagger/doc.json"))).Methods(http.MethodGet)
 	}
 	secured.Handle("/query", graphQLEndpoint)
 
