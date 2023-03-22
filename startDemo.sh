327721bac730e135fd21f7eca4470f1bf6b29b3a
Jan Eitzinger <jan.treibig@gmail.com>
2023-03-22 07:15:41 +0100

    Add validate option. Backport startDemo

--- startDemo.sh
+++ startDemo.sh
@@ -4,29 +4,17 @@ if [ -d './var' ]; then
     echo 'Directory ./var already exists! Skipping initialization.'
     ./cc-backend --server --dev
 else
-    mkdir ./var
-    cd ./var
+    make
 
+    cd var
     wget https://hpc-mover.rrze.uni-erlangen.de/HPC-Data/0x7b58aefb/eig7ahyo6fo2bais0ephuf2aitohv1ai/job-archive-dev.tar.xz
     tar xJf job-archive-dev.tar.xz
     rm ./job-archive-dev.tar.xz
+    cd ../
 
-    touch ./job.db
-    cd ../web/frontend
-    yarn install
-    yarn build
-
-    cd ../..
     cp ./configs/env-template.txt .env
     cp ./docs/config.json config.json
-    go build ./cmd/cc-backend
-<<<<<<< Updated upstream
 
+    ./cc-backend --migrate-db
     ./cc-backend --server --dev --init-db --add-user demo:admin:AdminDev
 fi
-
-=======
-
-    ./cc-backend --server --dev --init-db --add-user demo:admin:AdminDev
-fi
->>>>>>> Stashed changes
