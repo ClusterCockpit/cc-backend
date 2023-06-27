34050c8ce068520b26c0069c1c9737c397b39482
Jan Eitzinger <jan@moebiusband.org>
2023-06-27 14:30:09 +0200

    Fix argument dashes

--- startDemo.sh
+++ startDemo.sh
@@ -2,7 +2,7 @@
 
 if [ -d './var' ]; then
     echo 'Directory ./var already exists! Skipping initialization.'
-    ./cc-backend --server --dev
+    ./cc-backend -server -dev
 else
     make
     wget https://hpc-mover.rrze.uni-erlangen.de/HPC-Data/0x7b58aefb/eig7ahyo6fo2bais0ephuf2aitohv1ai/job-archive-demo.tar
@@ -12,6 +12,6 @@ else
     cp ./configs/env-template.txt .env
     cp ./configs/config-demo.json config.json
 
-    ./cc-backend --migrate-db
-    ./cc-backend --server --dev --init-db --add-user demo:admin:demo
+    ./cc-backend -migrate-db
+    ./cc-backend -server -dev -init-db -add-user demo:admin:demo
 fi
