bd6f38b4f374a2c4face165128f7d2d307de0ece
Christoph Kluge <christoph.kluge@fau.de>
2023-09-05 15:15:09 +0200

    job view layout fixes, fix polar plot axis scales

--- web/frontend/src/Job.root.svelte
+++ web/frontend/src/Job.root.svelte
@@ -272,8 +272,7 @@
         <Col />
     {/if}
 </Row>
-<br />
-<Row>
+<Row class="mb-3">
     <Col xs="auto">
         {#if $initq.data}
             <TagManagement job={$initq.data.job} bind:jobTags />
@@ -290,7 +289,6 @@
         <Zoom timeseriesPlots={plots} />
     </Col> -->
 </Row>
-<br />
 <Row>
     <Col>
         {#if $jobMetrics.error}
