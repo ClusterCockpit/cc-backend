e14d6a81fee95c80e43629a58c29df8d9fff9d1f
Christoph Kluge <christoph.kluge@fau.de>
2024-07-11 17:24:33 +0200

    fix: fix db migration to v8, changes key name to cpu_load

--- internal/repository/migrations/sqlite3/08_add-footprint.up.sql
+++ internal/repository/migrations/sqlite3/08_add-footprint.up.sql
@@ -5,7 +5,7 @@ UPDATE job SET footprint = '{"flops_any_avg": 0.0}';
 UPDATE job SET footprint = json_replace(footprint, '$.flops_any_avg', job.flops_any_avg);
 UPDATE job SET footprint = json_insert(footprint, '$.mem_bw_avg', job.mem_bw_avg);
 UPDATE job SET footprint = json_insert(footprint, '$.mem_used_max', job.mem_used_max);
-UPDATE job SET footprint = json_insert(footprint, '$.load_avg', job.load_avg);
+UPDATE job SET footprint = json_insert(footprint, '$.cpu_load_avg', job.load_avg);
 ALTER TABLE job DROP flops_any_avg;
 ALTER TABLE job DROP mem_bw_avg;
 ALTER TABLE job DROP mem_used_max;
