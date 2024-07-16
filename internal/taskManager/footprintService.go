b6f011c669455c8d93dbb4b4ef9f1b807dfb6253
Jan Eitzinger <jan@moebiusband.org>
2024-07-16 12:34:27 +0200

    Move footprint update task placeholder to taskmanager

--- internal/taskManager/footprintService.go
+++ internal/taskManager/footprintService.go
@@ -2,7 +2,7 @@
 // All rights reserved.
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
-package repository
+package taskManager
 
-func (r *JobRepository) footprintWorker() {
+func registerFootprintWorker() {
 }
