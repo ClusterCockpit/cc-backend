708eaf4178f6eb739478baeabef146def7cec100
Christoph Kluge <christoph.kluge@fau.de>
2024-08-22 17:55:21 +0200

    fix dev leftovers

--- web/frontend/src/job/Metric.svelte
+++ web/frontend/src/job/Metric.svelte
@@ -110,7 +110,7 @@
         client: client,
         query: subQuery,
         variables: { dbid, selectedMetrics, selectedScopes, selectedResolution },
-        requestPolicy:"network-only"
+        // requestPolicy:"network-only"
       });
 
       if ($metricData && !$metricData.fetching) {
