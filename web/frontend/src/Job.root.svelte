998ef8d834da572a6da93fc83f1c6e9dbd74ef92
Christoph Kluge <christoph.kluge@fau.de>
2025-02-19 16:40:25 +0100

    fix: use job_view_selectedMetrics config instead of iterating globalMetrics
    - Caveat: Minimal Defaultset needs to be generally available on all clusters

--- web/frontend/src/Job.root.svelte
+++ web/frontend/src/Job.root.svelte
@@ -151,12 +151,7 @@ const roofQuery = gql`
 
     const pendingMetrics = [
       ...(ccconfig[`job_view_selectedMetrics:${job.cluster}`] ||
-        $initq.data.globalMetrics.reduce((names, gm) => {
-          if (gm.availability.find((av) => av.cluster === job.cluster)) {
-            names.push(gm.name);
-          }
-          return names;
-        }, [])
+        ccconfig[`job_view_selectedMetrics`]
       ),
       ...(ccconfig[`job_view_nodestats_selectedMetrics:${job.cluster}`] ||
         ccconfig[`job_view_nodestats_selectedMetrics`]
