b9b84b797156b678e1c7bf6441507d1c587ace4b
Jan Eitzinger <jan@moebiusband.org>
2025-12-04 07:43:55 +0100

    Use --init flag in startDemo script

--- startDemo.sh
+++ startDemo.sh
@@ -7,16 +7,14 @@ if [ -d './var' ]; then
   ./cc-backend -server -dev
 else
   make
+  ./cc-backend --init
+  cp ./configs/config-demo.json config.json
+
   wget https://hpc-mover.rrze.uni-erlangen.de/HPC-Data/0x7b58aefb/eig7ahyo6fo2bais0ephuf2aitohv1ai/job-archive-demo.tar
   tar xf job-archive-demo.tar
   rm ./job-archive-demo.tar
 
-  cp ./configs/env-template.txt .env
-  cp ./configs/config-demo.json config.json
-
-  ./cc-backend -migrate-db
   ./cc-backend -dev -init-db -add-user demo:admin,api:demo
-
   ./cc-backend -server -dev
 
 fi
