d1a78c13a4f7b99dea3e44259fa5a46549460f2c
Jan Eitzinger <jan@moebiusband.org>
2025-12-17 08:38:14 +0100

    Make loglevel info default for demo

--- startDemo.sh
+++ startDemo.sh
@@ -4,7 +4,7 @@
 
 if [ -d './var' ]; then
   echo 'Directory ./var already exists! Skipping initialization.'
-  ./cc-backend -server -dev
+  ./cc-backend -server -dev -loglevel info
 else
   make
   ./cc-backend --init
@@ -15,5 +15,5 @@ else
   rm ./job-archive-demo.tar
 
   ./cc-backend -dev -init-db -add-user demo:admin,api:demo
-  ./cc-backend -server -dev
+  ./cc-backend -server -dev -loglevel info
 fi
