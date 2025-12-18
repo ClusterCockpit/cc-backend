d446c135468056f11df75d272964b375dc6bfa7a
Jan Eitzinger <jan@moebiusband.org>
2025-12-18 15:47:51 +0100

    Restore startDemo script

--- startDemo.sh
+++ startDemo.sh
@@ -1,22 +1,18 @@
 #!/bin/sh
 
-# rm -rf var
-
 if [ -d './var' ]; then
   echo 'Directory ./var already exists! Skipping initialization.'
-  ./cc-backend -server -dev
+  ./cc-backend -server -dev -loglevel info
 else
   make
-  wget https://hpc-mover.rrze.uni-erlangen.de/HPC-Data/0x7b58aefb/eig7ahyo6fo2bais0ephuf2aitohv1ai/job-archive-dev.tar
-  tar xf job-archive-dev.tar
-  rm ./job-archive-dev.tar
-
-  cp ./configs/env-template.txt .env
+  ./cc-backend --init
   cp ./configs/config-demo.json config.json
 
-  ./cc-backend -migrate-db
+  wget https://hpc-mover.rrze.uni-erlangen.de/HPC-Data/0x7b58aefb/eig7ahyo6fo2bais0ephuf2aitohv1ai/job-archive-demo.tar
+  tar xf job-archive-demo.tar
+  rm ./job-archive-demo.tar
+
   ./cc-backend -dev -init-db -add-user demo:admin,api:demo
-
-  ./cc-backend -server -dev
-
+  ./cc-backend -server -dev -loglevel info
 fi
+
